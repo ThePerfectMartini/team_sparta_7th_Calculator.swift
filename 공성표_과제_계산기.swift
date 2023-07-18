c4f8d550ae6e1cb0fe69d6f24909d3368bd0acdc
ThePerfectMartini <98042509+ThePerfectMartini@users.noreply.github.com>
2023-07-18 20:23:28 +0900

    추상화를 좀더 이해해본 뒤 수정
    튜터님의 도움을 받음

--- 공성표_과제_계산기.swift
+++ 공성표_과제_계산기.swift
@@ -1,20 +1,10 @@
-//
-//  공성표_과제_계산기.swift
-//  
-//
-//  Created by t2023-m0078 on 2023/07/18.
-//
-
-import Foundation
-
-
-
 func main(){
     var cal = Calculator()
     cal.execute(10, "+", 5)
     cal.execute(10, "-", 5)
     cal.execute(10, "*", 5)
     cal.execute(10, "/", 5)
+    cal.execute(10, "ㅋㅋ", 5)
 }
 
 class Calculator{
@@ -24,45 +14,72 @@ class Calculator{
     func execute(_ first:Double,_ ops:String, _ last:Double){
         switch ops {
         case "+":
-            print(AddOpteration(first,last).answer)
+            print(AddOpteration(first,last).calculate())
         case "-":
-            print(SubstractOpteration(first,last).answer)
+            print(SubstractOpteration(first,last).calculate())
         case "*":
-            print(MultiplyOpteration(first,last).answer)
+            print(MultiplyOpteration(first,last).calculate())
         case "/":
-            print(DivideOpteration(first,last).answer)
+            print(DivideOpteration(first,last).calculate())
         default:
             print("형식에 맞지 않음 : 오류")
         }
     }
-    
 }
+
+
 
 protocol AbstractOperation{
     var frontNumber:Double { get }
     var endNumber:Double { get }
+    init(_ first:Double,_ last:Double)
+    func calculate() -> Double
+
 }
 
-class Operations:AbstractOperation{
+class AddOpteration: AbstractOperation{
     var frontNumber: Double
     var endNumber: Double
-        init(_ first:Double,_ last:Double){
-            frontNumber = first
-            endNumber = last
-        }
+    required init(_ first:Double,_ last:Double){
+        frontNumber = first
+        endNumber = last
+    }
+    func calculate() -> Double {
+        return frontNumber + endNumber
+    }
 }
-
-class AddOpteration: Operations{
-    var answer:Double {return frontNumber + endNumber}
+class SubstractOpteration: AbstractOperation{
+    var frontNumber: Double
+    var endNumber: Double
+    required init(_ first:Double,_ last:Double){
+        frontNumber = first
+        endNumber = last
+    }
+    func calculate() -> Double {
+        return frontNumber - endNumber
+    }
 }
-class SubstractOpteration: Operations{
-    var answer:Double {return frontNumber - endNumber}
+class MultiplyOpteration: AbstractOperation{
+    var frontNumber: Double
+    var endNumber: Double
+    required init(_ first:Double,_ last:Double){
+        frontNumber = first
+        endNumber = last
+    }
+    func calculate() -> Double {
+        return frontNumber * endNumber
+    }
 }
-class MultiplyOpteration: Operations{
-    var answer:Double {return frontNumber * endNumber}
-}
-class DivideOpteration: Operations{
-    var answer:Double {return frontNumber / endNumber}
+class DivideOpteration: AbstractOperation{
+    var frontNumber: Double
+    var endNumber: Double
+    required init(_ first:Double,_ last:Double){
+        frontNumber = first
+        endNumber = last
+    }
+    func calculate() -> Double {
+        return frontNumber / endNumber
+    }
 }
 
 
